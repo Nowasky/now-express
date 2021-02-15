060b0fe92673198115bd433d4ca812c29608d8e3
Ademar Nowasky Junior <nowasky.jr@gmail.com>
2021-02-15 13:07:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,20 +6,6 @@ const app = express();
 
 const port = 5000;
 
-// Body parser
-app.use(express.urlencoded({ extended: false }));
-
-app.get("/", (req,res) => {
-  res.setHeader("Content-Security-Policy", "frame-src https://bit.ly; report-uri /report");
-  res.sendFile('views/index.html', {root: __dirname })
-});
-
-app.post("/report", (req,res) => {
-  console.log(req.body)
-  ws.send('CSP report: ' + req.body);
-  return res.send('CSP violation report received');
-});
-
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
@@ -37,7 +23,21 @@ wss.on('connection', (ws: WebSocket) => {
     ws.send('Hi there, I am a WebSocket server');
 });
 
+// Body parser
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req,res) => {
+  res.setHeader("Content-Security-Policy", "frame-src https://bit.ly; report-uri /report");
+  res.sendFile('views/index.html', {root: __dirname })
+});
+
+app.post("/report", (req,res) => {
+  console.log(req.body)
+  ws.send('CSP report: ' + req.body);
+  return res.send('CSP violation report received');
+});
+
 // Listen on port 5000
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is listening on port 5000`);
 });
