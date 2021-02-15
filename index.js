ab911de91d54a187548d936d1e2448033a7b6ca0
Ademar Nowasky Junior <nowasky.jr@gmail.com>
2021-02-15 13:19:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -26,17 +26,3 @@ var server = app.listen(port, () => {
 });
 
 const wss = new WebSocket.Server({ server });
-
-wss.on('connection', (ws: WebSocket) => {
-
-    //connection is up, let's add a simple simple event
-    ws.on('message', (message: string) => {
-
-        //log the received message and send it back to the client
-        console.log('received: %s', message);
-        ws.send(`Hello, you sent -> ${message}`);
-    });
-
-    //send immediatly a feedback to the incoming connection    
-    ws.send('Hi there, I am a WebSocket server');
-});
