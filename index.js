cb005db0687d5c0124ba40e5c68a9d872aa061bb
Ademar Nowasky Junior <nowasky.jr@gmail.com>
2021-02-15 13:00:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,21 +9,36 @@ const port = 5000;
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-// Body parser
-app.use(express.urlencoded({ extended: false }));
+wss.on('connection', (ws: WebSocket) => {
 
-app.get("/", (req,res) => {
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message: string) => {
+
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
+    });
+
+    //send immediatly a feedback to the incoming connection    
+    ws.send('Hi there, I am a WebSocket server');
+});
+
+
+// Body parser
+server.use(express.urlencoded({ extended: false }));
+
+server.get("/", (req,res) => {
   res.setHeader("Content-Security-Policy", "frame-src https://bit.ly; report-uri /report");
   res.sendFile('views/index.html', {root: __dirname })
 });
 
-app.post("/report", (req,res) => {
+server.post("/report", (req,res) => {
   console.log(req.body)
   ws.send('CSP report: ' + req.body);
   return res.send('CSP violation report received');
 });
 
 // Listen on port 5000
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is listening on port 5000`);
 });
