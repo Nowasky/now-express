9a2d7022b4b8a910247e378e103c7d67022dc397
Ademar Nowasky Junior <nowasky.jr@gmail.com>
2021-02-15 11:43:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,34 +6,14 @@ const port = 5000;
 // Body parser
 app.use(express.urlencoded({ extended: false }));
 
-// Home route
-/*app.get("/", (req, res) => {
-  res.send("Welcome to a basic express App");
-});*/
-
 app.get("/", (req,res) => {
   res.setHeader("Content-Security-Policy", "frame-src https://bit.ly; report-uri /report");
   res.sendFile('views/index.html', {root: __dirname })
-  //res.render("index");
 });
 
 app.post("/report", (req,res) => {
   console.log(req.body)
   return res.send('CSP fail: Report received');
-});
-
-// Mock API
-app.get("/users", (req, res) => {
-  res.json([
-    { name: "William", location: "Abu Dhabi" },
-    { name: "Chris", location: "Vegas" }
-  ]);
-});
-
-app.post("/user", (req, res) => {
-  const { name, location } = req.body;
-
-  res.send({ status: "User created", name, location });
 });
 
 // Listen on port 5000
