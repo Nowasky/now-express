9aff641789cf3145b5f6903c9cb251ee2ca2b846
Ademar Nowasky Junior <nowasky.jr@gmail.com>
2021-02-15 13:09:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,20 +9,6 @@ const port = 5000;
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', (ws: WebSocket) => {
-
-    //connection is up, let's add a simple simple event
-    ws.on('message', (message: string) => {
-
-        //log the received message and send it back to the client
-        console.log('received: %s', message);
-        ws.send(`Hello, you sent -> ${message}`);
-    });
-
-    //send immediatly a feedback to the incoming connection    
-    ws.send('Hi there, I am a WebSocket server');
-});
-
 // Body parser
 app.use(express.urlencoded({ extended: false }));
 
@@ -38,6 +24,6 @@ app.post("/report", (req,res) => {
 });
 
 // Listen on port 5000
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server is listening on port 5000`);
 });
